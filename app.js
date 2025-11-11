610fd7a94e661eb40a79efbcebed0159bb608c88
ZoharAIMax <zohar.bodinshtain@me.com>
2025-11-12 00:57:57 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const vocabulary = [
   {
     "english": "absorb",
@@ -12,792 +13,12 @@ const vocabulary = [
     "hebrew": "לפנות / למשוך תשומת לב"
   },
   {
-    "english": "artificially",
-    "hebrew": "באופן מלאכותי"
-  },
-  {
-    "english": "aspire",
-    "hebrew": "לשאוף"
-  },
-  {
-    "english": "atmosphere",
-    "hebrew": "אווירה"
-  },
-  {
-    "english": "attraction",
-    "hebrew": "משיכה"
-  },
-  {
-    "english": "automatically",
-    "hebrew": "באופן אוטומטי"
-  },
-  {
-    "english": "avoid",
-    "hebrew": "להימנע"
-  },
-  {
-    "english": "believe it or not",
-    "hebrew": "תאמין או לא"
-  },
-  {
-    "english": "brand",
-    "hebrew": "מותג"
-  },
-  {
-    "english": "cashier",
-    "hebrew": "קופאי/ת"
-  },
-  {
-    "english": "check in",
-    "hebrew": "להירשם / לבצע צ'ק-אין"
-  },
-  {
-    "english": "controversial",
-    "hebrew": "שנוי במחלוקת"
-  },
-  {
-    "english": "counteract",
-    "hebrew": "לנטרל / לפעול נגד"
-  },
-  {
-    "english": "critic",
-    "hebrew": "מבקר"
-  },
-  {
-    "english": "currently",
-    "hebrew": "כעת / כיום"
-  },
-  {
-    "english": "cyberbullying",
-    "hebrew": "בריונות רשת"
-  },
-  {
-    "english": "determine",
-    "hebrew": "לקבוע / להחליט"
-  },
-  {
-    "english": "developed",
-    "hebrew": "מפותח"
-  },
-  {
-    "english": "disaster",
-    "hebrew": "אסון"
-  },
-  {
-    "english": "download",
-    "hebrew": "להוריד (קובץ)"
-  },
-  {
-    "english": "efficient",
-    "hebrew": "יעיל"
-  },
-  {
-    "english": "elderly",
-    "hebrew": "קשיש / מבוגר"
-  },
-  {
-    "english": "exchange",
-    "hebrew": "להחליף"
-  },
-  {
-    "english": "expand",
-    "hebrew": "להרחיב"
-  },
-  {
-    "english": "experiment",
-    "hebrew": "ניסוי"
-  },
-  {
-    "english": "extreme",
-    "hebrew": "קיצוני"
-  },
-  {
-    "english": "face",
-    "hebrew": "להתמודד עם / פנים"
-  },
-  {
-    "english": "fame",
-    "hebrew": "תהילה"
-  },
-  {
-    "english": "financially",
-    "hebrew": "מבחינה כלכלית"
-  },
-  {
-    "english": "fingerprint",
-    "hebrew": "טביעת אצבע"
-  },
-  {
-    "english": "fire",
-    "hebrew": "אש / לפטר"
-  },
-  {
-    "english": "flood",
-    "hebrew": "שיטפון"
-  },
-  {
-    "english": "for the moment",
-    "hebrew": "לעת עתה"
-  },
-  {
-    "english": "frustration",
-    "hebrew": "תסכול"
-  },
-  {
-    "english": "get rid of",
-    "hebrew": "להיפטר מ־"
-  },
-  {
-    "english": "hardly",
-    "hebrew": "בקושי"
-  },
-  {
-    "english": "humanity",
-    "hebrew": "אנושות"
-  },
-  {
-    "english": "impact",
-    "hebrew": "השפעה"
-  },
-  {
-    "english": "implication",
-    "hebrew": "רמיזה / השלכה"
-  },
-  {
-    "english": "impressive",
-    "hebrew": "מרשים"
-  },
-  {
-    "english": "in place of",
-    "hebrew": "במקום"
-  },
-  {
-    "english": "in the first place",
-    "hebrew": "מלכתחילה"
-  },
-  {
-    "english": "in the long run",
-    "hebrew": "בטווח הארוך"
-  },
-  {
-    "english": "in theory",
-    "hebrew": "בתיאוריה"
-  },
-  {
-    "english": "increasingly",
-    "hebrew": "יותר ויותר"
-  },
-  {
-    "english": "innovation",
-    "hebrew": "חדשנות"
-  },
-  {
-    "english": "instant",
-    "hebrew": "מיידי"
-  },
-  {
-    "english": "interact",
-    "hebrew": "לתקשר"
-  },
-  {
-    "english": "item",
-    "hebrew": "פריט"
-  },
-  {
-    "english": "keep track of",
-    "hebrew": "לעקוב אחרי"
-  },
-  {
-    "english": "launch",
-    "hebrew": "להשיק / לשגר"
-  },
-  {
-    "english": "likely",
-    "hebrew": "סביר"
-  },
-  {
-    "english": "limited",
-    "hebrew": "מוגבל"
-  },
-  {
-    "english": "lower",
-    "hebrew": "להוריד / נמוך יותר"
-  },
-  {
-    "english": "make sense",
-    "hebrew": "להיות הגיוני"
-  },
-  {
-    "english": "marketing",
-    "hebrew": "שיווק"
-  },
-  {
-    "english": "more and more",
-    "hebrew": "יותר ויותר"
-  },
-  {
-    "english": "outcome",
-    "hebrew": "תוצאה"
-  },
-  {
-    "english": "particular",
-    "hebrew": "מסוים / מיוחד"
-  },
-  {
-    "english": "personally",
-    "hebrew": "באופן אישי"
-  },
-  {
-    "english": "planet",
-    "hebrew": "כוכב לכת"
-  },
-  {
-    "english": "practically",
-    "hebrew": "למעשה"
-  },
-  {
-    "english": "predict",
-    "hebrew": "לחזות / לנבא"
-  },
-  {
-    "english": "primarily",
-    "hebrew": "בעיקר"
-  },
-  {
-    "english": "priority",
-    "hebrew": "עדיפות"
-  },
-  {
-    "english": "private",
-    "hebrew": "פרטי"
-  },
-  {
-    "english": "process",
-    "hebrew": "תהליך / לעבד"
-  },
-  {
-    "english": "production",
-    "hebrew": "ייצור"
-  },
-  {
-    "english": "profession",
-    "hebrew": "מקצוע"
-  },
-  {
-    "english": "promising",
-    "hebrew": "מבטיח"
-  },
-  {
-    "english": "promote",
-    "hebrew": "לקדם"
-  },
-  {
-    "english": "pros and cons",
-    "hebrew": "יתרונות וחסרונות"
-  },
-  {
-    "english": "protest",
-    "hebrew": "למחות"
-  },
-  {
-    "english": "purchase",
-    "hebrew": "רכישה"
-  },
-  {
-    "english": "recycle",
-    "hebrew": "למחזר"
-  },
-  {
-    "english": "reflect",
-    "hebrew": "לשקף / להרהר"
-  },
-  {
-    "english": "relate to",
-    "hebrew": "להתייחס ל־"
-  },
-  {
-    "english": "representative",
-    "hebrew": "נציג"
-  },
-  {
-    "english": "research",
-    "hebrew": "מחקר"
-  },
-  {
-    "english": "reverse",
-    "hebrew": "להפוך / היפוך"
-  },
-  {
-    "english": "review",
-    "hebrew": "סקירה / לבחון מחדש"
-  },
-  {
-    "english": "revolution",
-    "hebrew": "מהפכה"
-  },
-  {
-    "english": "role",
-    "hebrew": "תפקיד"
-  },
-  {
-    "english": "run out of",
-    "hebrew": "להיגמר"
-  },
-  {
-    "english": "rural",
-    "hebrew": "כפרי"
-  },
-  {
-    "english": "self-service",
-    "hebrew": "שירות עצמי"
-  },
-  {
-    "english": "set an example",
-    "hebrew": "לשמש דוגמה"
-  },
-  {
-    "english": "significant",
-    "hebrew": "משמעותי"
-  },
-  {
-    "english": "society",
-    "hebrew": "חברה"
-  },
-  {
-    "english": "sponsor",
-    "hebrew": "נותן חסות"
-  },
-  {
-    "english": "spray",
-    "hebrew": "לרסס"
-  },
-  {
-    "english": "subscriber",
-    "hebrew": "מנוי"
-  },
-  {
-    "english": "surface",
-    "hebrew": "משטח / פני השטח"
-  },
-  {
-    "english": "survey",
-    "hebrew": "סקר"
-  },
-  {
-    "english": "take action",
-    "hebrew": "לנקוט פעולה"
-  },
-  {
-    "english": "take over",
-    "hebrew": "להשתלט"
-  },
-  {
-    "english": "take seriously",
-    "hebrew": "לקחת ברצינות"
-  },
-  {
-    "english": "the reality",
-    "hebrew": "המציאות"
-  },
-  {
-    "english": "theft",
-    "hebrew": "גניבה"
-  },
-  {
-    "english": "viewer",
-    "hebrew": "צופה"
-  },
-  {
-    "english": "virtually",
-    "hebrew": "למעשה / כמעט"
-  },
-  {
-    "english": "warehouse",
-    "hebrew": "מחסן"
-  },
-  {
-    "english": "while",
-    "hebrew": "בזמן ש־"
-  },
-  {
-    "english": "wise",
-    "hebrew": "חכם"
-  },
-  {
-    "english": "with regard to",
-    "hebrew": "בנוגע ל־"
-  },
-  {
-    "english": "you never know",
-    "hebrew": "לך תדע"
-  },
-  {
-    "english": "agenda",
-    "hebrew": "סדר יום"
-  },
-  {
     "english": "agriculture",
     "hebrew": "חקלאות"
   },
   {
-    "english": "approach",
-    "hebrew": "לגשת / גישה"
-  },
-  {
-    "english": "appropriate",
-    "hebrew": "מתאים"
-  },
-  {
-    "english": "assistance",
-    "hebrew": "עזרה / סיוע"
-  },
-  {
-    "english": "assume",
-    "hebrew": "להניח"
-  },
-  {
-    "english": "backpacking",
-    "hebrew": "טיול תרמילאים"
-  },
-  {
-    "english": "be sentenced to",
-    "hebrew": "להישפט / לקבל עונש"
-  },
-  {
-    "english": "betray",
-    "hebrew": "לבגוד"
-  },
-  {
-    "english": "bravery",
-    "hebrew": "אומץ"
-  },
-  {
-    "english": "break out",
-    "hebrew": "לפרוץ"
-  },
-  {
-    "english": "bribe",
-    "hebrew": "שוחד / לשחד"
-  },
-  {
-    "english": "brick",
-    "hebrew": "לבנה"
-  },
-  {
-    "english": "bury",
-    "hebrew": "לקבור"
-  },
-  {
-    "english": "bystander",
-    "hebrew": "עובר אורח / עד ראייה"
-  },
-  {
-    "english": "carpenter",
-    "hebrew": "נגר"
-  },
-  {
-    "english": "circumstance",
-    "hebrew": "נסיבות"
-  },
-  {
-    "english": "coffin",
-    "hebrew": "ארון קבורה"
-  },
-  {
-    "english": "commander",
-    "hebrew": "מפקד"
-  },
-  {
-    "english": "commendable",
-    "hebrew": "ראוי לשבח"
-  },
-  {
-    "english": "compassionate",
-    "hebrew": "רחום"
-  },
-  {
-    "english": "conscience",
-    "hebrew": "מצפון"
-  },
-  {
-    "english": "cooperation",
-    "hebrew": "שיתוף פעולה"
-  },
-  {
-    "english": "countless",
-    "hebrew": "אינספור"
-  },
-  {
-    "english": "courageous",
-    "hebrew": "אמיץ"
-  },
-  {
-    "english": "dedicated",
-    "hebrew": "מסור"
-  },
-  {
-    "english": "departure",
-    "hebrew": "עזיבה"
-  },
-  {
-    "english": "desperately",
-    "hebrew": "בנואשות"
-  },
-  {
-    "english": "destruction",
-    "hebrew": "הרס"
-  },
-  {
-    "english": "disloyal",
-    "hebrew": "לא נאמן"
-  },
-  {
-    "english": "drown",
-    "hebrew": "לטבוע"
-  },
-  {
-    "english": "drug",
-    "hebrew": "סם"
-  },
-  {
-    "english": "duration",
-    "hebrew": "משך זמן"
-  },
-  {
-    "english": "empathy",
-    "hebrew": "אמפתיה"
-  },
-  {
-    "english": "enable",
-    "hebrew": "לאפשר"
-  },
-  {
-    "english": "escape",
-    "hebrew": "לברוח"
-  },
-  {
-    "english": "exceptional",
-    "hebrew": "יוצא דופן"
-  },
-  {
-    "english": "expose",
-    "hebrew": "לחשוף"
-  },
-  {
-    "english": "fluent",
-    "hebrew": "שוטף"
-  },
-  {
-    "english": "force",
-    "hebrew": "כוח / להכריח"
-  },
-  {
-    "english": "former",
-    "hebrew": "לשעבר"
-  },
-  {
-    "english": "funding",
-    "hebrew": "מימון"
-  },
-  {
-    "english": "generation",
-    "hebrew": "דור"
-  },
-  {
-    "english": "give birth",
-    "hebrew": "ללדת"
-  },
-  {
-    "english": "hatred",
-    "hebrew": "שנאה"
-  },
-  {
-    "english": "heroism",
-    "hebrew": "גבורה"
-  },
-  {
-    "english": "immigrant",
-    "hebrew": "מהגר"
-  },
-  {
-    "english": "implement",
-    "hebrew": "ליישם"
-  },
-  {
-    "english": "in charge of",
-    "hebrew": "אחראי על"
-  },
-  {
-    "english": "investigate",
-    "hebrew": "לחקור"
-  },
-  {
-    "english": "lead a quiet life",
-    "hebrew": "לחיות חיים שקטים"
-  },
-  {
-    "english": "make a difference",
-    "hebrew": "לעשות שינוי"
-  },
-  {
-    "english": "make an impression",
-    "hebrew": "להשאיר רושם"
-  },
-  {
-    "english": "mandatory",
-    "hebrew": "חובה"
-  },
-  {
-    "english": "media",
-    "hebrew": "תקשורת"
-  },
-  {
-    "english": "miracle",
-    "hebrew": "נס"
-  },
-  {
-    "english": "mission",
-    "hebrew": "משימה"
-  },
-  {
-    "english": "noble",
-    "hebrew": "אצילי"
-  },
-  {
-    "english": "nominate",
-    "hebrew": "למנות"
-  },
-  {
-    "english": "nursing home",
-    "hebrew": "בית אבות"
-  },
-  {
-    "english": "observe",
-    "hebrew": "להתבונן"
-  },
-  {
-    "english": "obtain",
-    "hebrew": "להשיג"
-  },
-  {
-    "english": "operation",
-    "hebrew": "ניתוח / מבצע"
-  },
-  {
-    "english": "organization",
-    "hebrew": "ארגון"
-  },
-  {
-    "english": "overcome",
-    "hebrew": "להתגבר"
-  },
-  {
-    "english": "participant",
-    "hebrew": "משתתף"
-  },
-  {
-    "english": "pass away",
-    "hebrew": "למות"
-  },
-  {
-    "english": "penalty",
-    "hebrew": "עונש"
-  },
-  {
-    "english": "perish",
-    "hebrew": "להיכחד"
-  },
-  {
-    "english": "personal hygiene",
-    "hebrew": "היגיינה אישית"
-  },
-  {
-    "english": "phenomenon",
-    "hebrew": "תופעה"
-  },
-  {
-    "english": "precious",
-    "hebrew": "יקר"
-  },
-  {
-    "english": "pretend",
-    "hebrew": "להעמיד פנים"
-  },
-  {
-    "english": "prevent",
-    "hebrew": "למנוע"
-  },
-  {
-    "english": "psychology",
-    "hebrew": "פסיכולוגיה"
-  },
-  {
-    "english": "raise",
-    "hebrew": "להרים"
-  },
-  {
-    "english": "react",
-    "hebrew": "להגיב"
-  },
-  {
-    "english": "remarkable",
-    "hebrew": "יוצא דופן"
-  },
-  {
-    "english": "rescue",
-    "hebrew": "להציל"
-  },
-  {
-    "english": "response",
-    "hebrew": "תגובה"
-  },
-  {
-    "english": "restore",
-    "hebrew": "לשקם"
-  },
-  {
-    "english": "reunite",
-    "hebrew": "להתאחד"
-  },
-  {
-    "english": "risk one’s life",
-    "hebrew": "לסכן את חייו"
-  },
-  {
-    "english": "shelter",
-    "hebrew": "מקלט"
-  },
-  {
-    "english": "smuggle",
-    "hebrew": "להבריח"
-  },
-  {
-    "english": "support",
-    "hebrew": "תמיכה"
-  },
-  {
-    "english": "take advantage of",
-    "hebrew": "לנצל"
-  },
-  {
-    "english": "tendency",
-    "hebrew": "נטייה"
-  },
-  {
-    "english": "torture",
-    "hebrew": "עינוי"
-  },
-  {
-    "english": "trace",
-    "hebrew": "עקבות / לעקוב"
-  },
-  {
-    "english": "unique",
-    "hebrew": "ייחודי"
-  },
-  {
-    "english": "victim",
-    "hebrew": "קורבן"
+    "english": "agenda",
+    "hebrew": "סדר יום"
   }
 ];
 
@@ -811,6 +32,7 @@ const wordEnEl = document.getElementById("word-en");
 const wordHeEl = document.getElementById("word-he");
 const startBtn = document.getElementById("startBtn");
 const showBtn = document.getElementById("showBtn");
+const listenBtn = document.getElementById("listenBtn");
 const knewBtn = document.getElementById("knewBtn");
 const didntBtn = document.getElementById("didntBtn");
 const restartBtn = document.getElementById("restartBtn");
@@ -840,12 +62,14 @@ function resetSession(useMistakesOnly = false) {
   wordEnEl.textContent = "Ready – press “Start” to begin";
   wordHeEl.textContent = "—";
   wordHeEl.classList.add("hidden");
+  listenBtn.disabled = true;
 }
 
 function showNextWord() {
   if (order.length === 0) {
     wordEnEl.textContent = "No words loaded.";
     wordHeEl.textContent = "—";
+    listenBtn.disabled = true;
     return;
   }
   currentIndex++;
@@ -853,6 +77,7 @@ function showNextWord() {
     wordEnEl.textContent = "Session complete!";
     wordHeEl.textContent = "You can restart or switch modes.";
     wordHeEl.classList.remove("hidden");
+    listenBtn.disabled = true;
     return;
   }
   const idx = order[currentIndex];
@@ -861,6 +86,7 @@ function showNextWord() {
   wordHeEl.textContent = entry.hebrew;
   wordHeEl.classList.add("hidden");
   totalSeen++;
+  listenBtn.disabled = false;
   updateStats();
 }
 
@@ -869,6 +95,25 @@ function updateStats() {
   progressText.textContent = total ? `${Math.min(totalSeen, total)} / ${total}` : "0 / 0";
   const score = totalSeen > 0 ? Math.round((knownCount / totalSeen) * 100) : 0;
   scoreText.textContent = `${score}%`;
+}
+
+// פונקציית השמעה באמצעות Web Speech API
+function speakCurrentWord() {
+  if (!("speechSynthesis" in window)) {
+    alert("Speech is not supported in this browser.");
+    return;
+  }
+  if (currentIndex < 0 || currentIndex >= order.length) {
+    return;
+  }
+  const idx = order[currentIndex];
+  const text = vocabulary[idx].english;
+  const utterance = new SpeechSynthesisUtterance(text);
+  utterance.lang = "en-US";
+  utterance.rate = 1.0;
+  utterance.pitch = 1.0;
+  window.speechSynthesis.cancel();
+  window.speechSynthesis.speak(utterance);
 }
 
 startBtn.addEventListener("click", () => {
@@ -883,6 +128,10 @@ startBtn.addEventListener("click", () => {
 
 showBtn.addEventListener("click", () => {
   wordHeEl.classList.remove("hidden");
+});
+
+listenBtn.addEventListener("click", () => {
+  speakCurrentWord();
 });
 
 knewBtn.addEventListener("click", () => {
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mia-vocab-cache-v1";
+const CACHE_NAME = "mia-vocab-cache-v1-1";
 const ASSETS = [
   "./",
   "./index.html",
